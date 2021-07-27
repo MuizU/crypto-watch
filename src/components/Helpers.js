6abf876088b1f25bec7f149d1d7cef11276a6f48
MuizU <muizuvais99@gmail.com>
2021-07-27 17:34:42 +0530

    added new helper functiuon for id to name conversion

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -1,5 +1,8 @@
 export const roundOff = (value) => {
   if (!isNaN(value)) {
+    if (Math.round(value * 100) / 100 <= 0) {
+      return value;
+    }
     return Math.round(value * 100) / 100;
   }
   return value;
@@ -11,3 +14,15 @@ export const checkNegative = (value) => {
   }
   return false;
 };
+
+export const getCurrencyNameFromId = (id) => {
+  if (typeof id === "string") {
+    return capitalizeEachWord(id.replace(/-/g, " "));
+  }
+  return "";
+};
+
+export const capitalizeEachWord = (word) =>
+  word.replace(/(?:^|\s)\S/g, function (a) {
+    return a.toUpperCase();
+  });
