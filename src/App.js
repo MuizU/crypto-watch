ab2d340cbc3d26bd7ae5d8ea50e698ec75dd4fe8
MuizU <muizuvais99@gmail.com>
2021-07-29 16:42:09 +0530

    added a suspense component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React, { Suspense } from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import Spinner from "./components/layout/Spinner";
 import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
       <>
-        <Navigation></Navigation>
+        <Suspense
+          fallback={
+            <div>
+              <Spinner />
+            </div>
+          }
+        >
+          <Navigation />
+        </Suspense>
         <Footer />
       </>
     </Router>
