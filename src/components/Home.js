b23da4d186cc318440a3105ce1b91aa80fabb031
MuizU <muizuvais99@gmail.com>
2021-07-30 14:44:57 +0530

    added composition to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import CryptoCard from "./CryptoCard";
+import DisplayCard from "./DisplayCard";
 import axios from "axios";
 import Spinner from "./layout/Spinner";
+import { roundOff } from "./Helpers";
+import { Link } from "react-router-dom";
 export default class Home extends Component {
   constructor(props) {
     super(props);
@@ -28,7 +30,28 @@ export default class Home extends Component {
           <div className="container">
             <div className="row pt-4 mb-5">
               {coinDetails.map((detail, i) => (
-                <CryptoCard key={i} detail={detail}></CryptoCard>
+                <DisplayCard
+                  detail={detail}
+                  key={i}
+                  cardText={
+                    <span>
+                      Supply: {roundOff(detail.supply)}
+                      <br />
+                      Price: &#36;{roundOff(detail.priceUsd)}
+                      <br />
+                      Market Cap: &#36;
+                      {roundOff(detail.marketCapUsd)}
+                    </span>
+                  }
+                  btn={
+                    <Link
+                      to={`/coin/${detail.id}`}
+                      className="btn btn-warning btn-block"
+                    >
+                      View {detail.name}
+                    </Link>
+                  }
+                />
               ))}
             </div>
           </div>
