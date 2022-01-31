a08db7a1658ff902d524899b747f7062e8e6e10a
MuizU <muizuvais99@gmail.com>
2022-01-31 16:59:10 +0530

    added toastify configs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import "./App.css";
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./Navigation";
 import Spinner from "./layout/Spinner";
 import Footer from "./layout/Footer";
-
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
         >
           <Navigation />
         </Suspense>
+        <ToastContainer />
         <Footer />
       </>
     </Router>
